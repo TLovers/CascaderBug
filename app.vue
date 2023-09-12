52d35da8e2fadc5b7d69b0130a9489ffea577cca
TLovers <879641161@qq.com>
2023-09-12 20:00:44 +0800

    bug复现

---
+++ app.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import 'ant-design-vue/dist/antd.css';
+import {Cascader} from 'ant-design-vue'
+import type { CascaderProps } from 'ant-design-vue';
+const options: CascaderProps['options'] = [
+  {
+    value: 'zhejiang',
+    label: 'Zhejiang',
+    children: [
+      {
+        value: 'hangzhou',
+        label: 'Hangzhou',
+        children: [
+          {
+            value: 'xihu',
+            label: 'West Lake',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    value: 'jiangsu',
+    label: 'Jiangsu',
+    children: [
+      {
+        value: 'nanjing',
+        label: 'Nanjing',
+        children: [
+          {
+            value: 'zhonghuamen',
+            label: 'Zhong Hua Men',
+          },
+        ],
+      },
+    ],
+  },
+];
+const value =ref<string[]>([])
+</script>
+<template>
+  <div>
+    <Cascader v-model:value="value" :options="options" placeholder="Please select" />
+  </div>
+</template>
